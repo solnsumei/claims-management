e14eca71a4a868ea6cc7dff4ea51d7feb694d826
solnsumei <solnsumei@gmail.com>
2021-01-28 17:22:50 +0100

    Remove cyclic fk constraints

--- src/models/department.py
+++ src/models/department.py
@@ -3,7 +3,7 @@ from .base.basemodel import ModelWithStatus, fields
 
 class Department(ModelWithStatus):
     name = fields.CharField(max_length=70, unique=True)
-    manager = fields.ForeignKeyField('models.User', related_name='departments', null=True)
+    # manager = fields.ForeignKeyField('models.User', related_name='departments', null=True)
 
     class Meta:
         table = 'departments'
