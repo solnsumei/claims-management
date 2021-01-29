428703a9d3ae0f792dfa207638ce6445ebc6d069
solnsumei <solnsumei@gmail.com>
2021-01-29 08:56:26 +0100

    Refactor authentication process and retrieval

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -14,3 +14,18 @@ class UnauthorisedException(HTTPException):
             detail=message or "Access token is missing or invalid",
             headers={"WWW-Authenticate": "Bearer"}
         )
+
+
+class ForbiddenException(HTTPException):
+    """Exception raised for authorization errors.
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message: str = None):
+        super().__init__(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=message or "You do not have access to this resource",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
