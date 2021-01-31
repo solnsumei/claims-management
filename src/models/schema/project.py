ba0de28d0743be0eaf135814035e5d88bcfa7bfd
solnsumei <solnsumei@gmail.com>
2021-01-31 05:26:52 +0100

    Add update schema to projects

--- src/models/schema/project.py
+++ src/models/schema/project.py
@@ -3,9 +3,18 @@ from pydantic import Field, UUID4
 from .baseschema import NameDescriptionSchema
 
 
-class ProjectSchema(NameDescriptionSchema):
+class CreateSchema(NameDescriptionSchema):
     code: str = Field(..., min_length=2, max_length=15)
     manager_id: UUID4
     budget: int = Field(..., gt=0)
     duration: int = Field(..., gt=0)
+    department_id: Optional[UUID4]
+
+
+class UpdateSchema(NameDescriptionSchema):
+    name: Optional[str] = Field(None, min_length=3, max_length=70, description="Name is required")
+    description: Optional[str]
+    manager_id: Optional[UUID4]
+    budget: Optional[int] = Field(None, gt=0)
+    duration: Optional[int] = Field(None, gt=0)
     department_id: Optional[UUID4]
